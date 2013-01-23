8867b30d8d847d0a61a65e0e3f75e175032133f7
amy wieliczka <amywieliczka@berkeley.edu>
2013-01-23 13:32:14 -0800

    NOJIRA: investigating how to add a click event handler to a button

--- src/main/webapp/tenants/botgarden/js/GeorefIntegration.js
+++ src/main/webapp/tenants/botgarden/js/GeorefIntegration.js
@@ -18,7 +18,7 @@ var cspace = cspace || {};
     fluid.defaults("cspace.georefIntegration", {
         gradeNames: ["fluid.rendererComponent", "autoInit"],
         selectors: {
-			georef_Btn: ".csc-georef-btn",
+			georefBtn: ".csc-georef-btn",
             georefSrc: ".csc-georef-src .content textarea",
 			
 			georefLatitude: ".csc-georef-lat",
@@ -51,57 +51,22 @@ var cspace = cspace || {};
 	cspace.georefIntegration.produceTree = function (that) {
 		return {
 			georefSrc: "${source}",
-            // georefLatitude: "${latitude}",
-            // georefLongitude: "${longitude}",
-            // georefDatum: {
-            //     optionnames: ["Please select a value", "Not Recorded", "ADG66", "NAD27", "NAD83", "NAD83&WGS84", "WGS84"],
-            //     optionlist: ["", "Not Recorded", "ADG66", "NAD27", "NAD83", "NAD83&WGS84", "WGS84"],
-            //     selection: "${datum}"
-            // },
-            // georefUncertainty: "${uncertainty}",
-            // georefUncertaintyUnits: {
-            //     optionnames: ["Please select a value", "unknown", "feet", "kilometers", "meters", "miles"],
-            //     optionlist: ["", "unknown", "feet", "kilometers", "meters", "miles"],
-            //     selection: "${uncertaintyUnits}"
-            // },
-            // georefProtocol: {
-            //     optionnames: ["Please select a value", "Chapman, Wieczorek 2006, Guide to Best Practices for Georeferencing", "MaNIS/HerpNet/ORNIS Georeferencing Guidelines", "Georeferencing For Dummies", "BioGeomancer", "Google Maps GeoCoding Service API v3"],
-            //     optionlist: ["", "chapman-wieczorek-2006-guide-best-practices-georeferencing", "manis-herpnet-ornis-georeferencing-guidelines", "georeferencing-dummies", "biogeomancer", "Google Maps GeoCoding Service API v3"],
-            //     selection: "${protocol}"
-            // },
-            // georefRemarks: "${remarks}",
-			georef_Btn: {
+			georefBtn: {
 				decorators: [{
 					type: "jQuery",
 					func: "click",
 					args: (
-					    function() { 
-					        alert('Hello world!');
-                            // georefjs.googleGeoref(that.model.source, function(val) {
-                            //     console.log(val[0]);
-                            //                                 that.applier.requestChange("latitude", val[0].decimalLatitude);
-                            //                                 that.applier.requestChange("longitude", val[0].decimalLongitude);
-                            //                                 that.applier.requestChange("datum", val[0].geodeticDatum);
-                            //                                 that.applier.requestChange("uncertainty", val[0].coordinateUncertaintyInMeters);
-                            //                                 that.applier.requestChange("uncertaintyUnits", "meters");
-                            //                                 that.applier.requestChange("protocol", val[0].georeferenceProtocol);                             
-                            //                                 that.applier.requestChange("remarks", val[0].georeferenceRemarks);
-                            //                                 that.refreshView();
-                            //                                 $(that.options.selectors.georefLatitude).css({"background": "#fffbbb"});
-                            //                                 $(that.options.selectors.georefLongitude).css({"background": "#fffbbb"});
-                            //                                 $(that.options.selectors.georefDatum).css({"background": "#fffbbb"});                                
-                            //                                 $(that.options.selectors.georefUncertainty).css({"background": "#fffbbb"});
-                            //                                 $(that.options.selectors.georefUncertaintyUnits).css({"background": "#fffbbb"});
-                            //                                 $(that.options.selectors.georefProtocol).css({"background": "#fffbbb"});
-                            //                                 $(that.options.selectors.georefRemarks).css({"background": "#fffbbb"});
-                            // });
-					    }
+                        function() {
+                            console.log("clicky");
+                        }
 					)
 				}]
 			}
 		};
 	}
 	
-    cspace.georefIntegration.finalInit = function (that) {};
+    cspace.georefIntegration.finalInit = function (that) {
+        $(that.options.selectors.georefBtn).css("color", "purple").click(function() { console.log("clicky") });
+    };
 		
 })(jQuery, fluid);
